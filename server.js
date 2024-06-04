632230f0a5009f40b84ae683de9671be4dfe63ae
vyom-cpp <vyom.sutariya0948@gmail.com>
2024-06-04 19:50:03 +0530

    code update

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const contactRoutes = require('./routes/contact');
-require('dotenv').config(); // Load environment variables
 
 const app = express();
-const PORT = process.env.PORT || 5000; // Use PORT from environment or default to 5000
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors()); // Enable CORS
@@ -14,7 +13,7 @@ app.use(bodyParser.json());
 app.use('/api', contactRoutes);
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
