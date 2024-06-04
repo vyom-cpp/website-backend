6d52ee8d4fadeb6d39310bfde7ddff1ef9be1060
vyom-cpp <vyom.sutariya0948@gmail.com>
2024-06-04 21:25:11 +0530

    added .env configuration

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const contactRoutes = require('./routes/contact');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
